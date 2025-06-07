59348a66e55c93a00b27b482b0bd990b5cdd6a71
HijjazD <154423459+HijjazD@users.noreply.github.com>
2025-06-07 18:51:20 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import PollDetailsPage from "./pages/PollDetailsPage"
 import ForgotPasswordPage from "./pages/ForgotPasswordPage"
 import LoadingSpinner from "./components/LoadingSpinner"
 import ResetPasswordPage from "./pages/ResetPasswordPage"
+import { Toaster } from "react-hot-toast";
 
 import { useAuthStore } from "./store/authStore"
 import { useEffect } from "react"
@@ -58,12 +59,14 @@ function App() {
 
   return (
     <div className='min-h-screen bg-gradient-to-br
-    from-gray-600 via-gray-800 to-rose-900 relative overflow-hidden'>
+    from-gray-900 via-gray-800 to-gray-700 relative overflow-hidden'>
 
-      <FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
-			<FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
-			<FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
+      <FloatingShape color='bg-lime-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+	<FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
+	<FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
       <FloatingShape color='bg-yellow-500' size='w-32 h-32' top='50%' left='70%' delay={2} />
+
+      <Toaster position="top-right" reverseOrder={false} />
       <div className="relative z-10">
         <Routes>
           <Route path='/' element={
