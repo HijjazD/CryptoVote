f036454725514a4bb213b6d1bd794262e98f29f6
HijjazD <154423459+HijjazD@users.noreply.github.com>
2025-06-27 02:05:35 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,7 @@ import { useEffect } from "react"
 
 
 
+
 //Protect route that require authentication
 const ProtectedRoute = ({children}) => {
 
@@ -60,8 +61,8 @@ function App() {
     from-gray-900 via-gray-800 to-gray-700 relative overflow-hidden'>
 
       <FloatingShape color='bg-lime-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
-	<FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
-	<FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
+			<FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
+			<FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
       <FloatingShape color='bg-yellow-500' size='w-32 h-32' top='50%' left='70%' delay={2} />
 
       <Toaster position="top-right" reverseOrder={false} />
