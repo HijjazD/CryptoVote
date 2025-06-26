9b84e1c5cb55bc2aa05d17866c98686aaefb1950
HijjazD <154423459+HijjazD@users.noreply.github.com>
2025-06-26 21:29:02 +0800

    Create index.jsx

---
+++ frontend/src/context/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { wagmiAdapter, projectId, networks } from '../config'
+import { createAppKit } from '@reown/appkit'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+
+const queryClient = new QueryClient()
+
+const metadata = {
+  name: 'CryptoVote',
+  description: 'A decentralized student voting system',
+  url: 'https://www.cryptovote.online', // must be a real URL in production
+  icons: ['https://i.imgur.com/SrbkO1W.png'] // recommended to be square
+}
+
+
+const modal = createAppKit({
+  adapters: [wagmiAdapter],
+  projectId,
+  networks,
+  defaultNetwork: networks[1], // sepolia
+  metadata,
+  features: {
+    analytics: true,
+    email: true,
+    socials: ['google', 'x'],
+    emailShowWallets: true,
+  },
+  themeMode: 'dark',
+})
+
+function ContextProvider({ children }) {
+  return (
+    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+export default ContextProvider
