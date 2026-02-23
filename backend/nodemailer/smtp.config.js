7591acc2d4e26db1516bfaa8e51bd3af22ae6eb8
HijjazD <154423459+HijjazD@users.noreply.github.com>
2026-02-23 17:44:43 +0800

    Update smtp.config.js

--- backend/nodemailer/smtp.config.js
+++ backend/nodemailer/smtp.config.js
@@ -5,13 +5,12 @@ dotenv.config()
 
 export const transporter = nodemailer.createTransport({
   host: 'smtp-relay.brevo.com',
-  port: 2525,
+  port: 587,
   secure: false, // use TLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  tls: { rejectUnauthorized: false }
-
 });
+
 
